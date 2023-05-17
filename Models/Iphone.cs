6f2b2b9c6a98f555b45f0e2d279f8f094900f762
lucascidade <lucascidade96@gmail.com>
2023-05-17 12:32:11 -0400

    Desafio Concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // TODO: Herdar da classe "Smartphone" /FEITO!
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        // TODO: Sobrescrever o método "InstalarAplicativo" /FEITO!
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} ... ");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia telefone1 = new Nokia(numero: "9292929", modelo: "Nokia E5", imei: "1925612121", memoria: 8);
+Console.WriteLine($"Telefone Nokia: {telefone1.Numero}");
+telefone1.Ligar();
+telefone1.InstalarAplicativo("WHATSAPP");
+
+Console.WriteLine("\n");
+
+Iphone telefone2 = new Iphone(numero: "9393939", modelo: "iPhone 8s PLUS", imei: "35628134", memoria: 128);
+Console.WriteLine($"Telefone iPhone: {telefone2.Numero}");
+telefone2.ReceberLigacao();
+telefone2.InstalarAplicativo("TELEGRAM");
